436b31ec34d1fc33938f90531b052a5e2eb3a288
dawn <5ekdmsdldl@gmail.com>
2023-06-05 15:58:51 +0900

    [Feature & Refactoring] Read Drive

--- BestReview/DeviceDriver.h
+++ BestReview/DeviceDriver.h
@@ -5,9 +5,12 @@ class DeviceDriver
 {
 public:
     DeviceDriver(FlashMemoryDevice* hardware);
+    int readFromDevice(long address);
     int read(long address);
     void write(long address, int data);
 
 protected:
     FlashMemoryDevice* m_hardware;
+private:
+    const int READ_TRYCNT = 5;
 };
--- BestReview/FlashMemoryDevice.h
+++ BestReview/FlashMemoryDevice.h
@@ -1,6 +1,4 @@
 #pragma once
-#pragma once
-
 /*
  * This class represents the interface to a Flash Memory Device. The hardware has only two methods - 'read' and 'write'
  * However, the interface for using the device is a lot more complex than that. It is outlined in the top-level README file.
